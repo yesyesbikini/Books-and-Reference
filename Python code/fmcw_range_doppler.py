6b1e3e508291cbf24acf7f2847b3fbe33b055f79
yesyesbikini <60005280+yesyesbikini@users.noreply.github.com>
2021-02-14 10:49:25 +0100

    Add files via upload

--- Python code/fmcw_range_doppler.py
+++ Python code/fmcw_range_doppler.py
@@ -14,11 +14,11 @@ c0 = 3e8 # [m/s]
 r0 = 5 # [m]
 v0 = 14 # [m/s]
 
-ts = 1e-6 # sampling time [sec]
-
 f0 = 76e9 # [Hz]
 B = 0.5e9 # [Hz]
 N1 = 128 # no. of samples in fast time
+fs = B/N1
+ts = 1/fs
 z_fast = 4*N1
 N2 = 128 # no. of chirps in slow time
 z_slow = 4*N2
@@ -28,6 +28,18 @@ mu = B/T1
 #%% total time of n2 in slow time, constant r * v
 # for each n1 in fast time and each n2 in slow time
 s = np.zeros((N2, N1), dtype=np.complex64)
+
+r_res = c0/(2*B)
+r_vec = np.linspace(0, 1-1/z_fast, z_fast-1)* (N1-1)*r_res
+v_res = (c0/f0)/(2*N2*T1)
+v_vec = np.linspace(0, 1-1/z_slow, z_slow-1)* (N2-1)*v_res
+
+win_r = np.hanning(N1)
+win_r = np.tile(win_r, (N2,1))
+
+win_d = np.hanning(N2)
+win_d = np.tile(win_d, (z_fast,1)).T
+
 for n2 in range(N2):
     for n1 in range(N1):
         tau = 2*(r0 + v0*T1*n2 + v0*n1*ts) / c0
@@ -37,26 +49,9 @@ for n2 in range(N2):
                 )
             )
 
-r_res = c0/(2*B)
-r_vec = np.linspace(0, 1-1/z_fast, z_fast-1)*(N1-1)*r_res
-v_res = (c0/f0)/(2*N2*T1)
-v_vec = np.linspace(0, 1-1/z_slow, z_slow-1)*(N2-1)*v_res
-
-win_r = np.hanning(N1)
-win_r = np.tile(win_r, (N2,1))
 rp = np.fft.fft(s*win_r, n=z_fast, axis=1)
 rp_abs = np.abs(rp)
 
-# fig, ax = plt.subplots(figsize=(6,6))
-# ax.imshow(rp_abs/np.max(rp_abs), 
-#           cmap=plt.cm.Reds, 
-#           interpolation='none', 
-#           extent=[r_vec[0], r_vec[-1], v_vec[0], v_vec[-1]],
-#           aspect='auto',
-#           origin='lower')
-
-win_d = np.hanning(N2)
-win_d = np.tile(win_d, (z_fast,1)).T
 rd = np.fft.fft(rp*win_d, n=z_slow, axis=0)
 rd_abs = np.abs(rd)
 
@@ -113,16 +108,9 @@ for i_T1 in range(t_list.shape[0]):
 
 win_r = np.hanning(N1)
 win_r = np.tile(win_r, (t_list.shape[0],1))
+
 rp1 = np.fft.fft(s1*win_r, n=z_fast, axis=1)
 rp1_abs = np.abs(rp1)
-
-# fig, ax = plt.subplots(figsize=(6,6))
-# ax.imshow(rp_abs/np.max(rp_abs), 
-#           cmap=plt.cm.Reds, 
-#           interpolation='none', 
-#           extent=[r_vec[0], r_vec[-1], v_vec[0], v_vec[-1]],
-#           aspect='auto',
-#           origin='lower')
 
 rd1 = np.fft.fft(rp1[0*N2:1*N2,:]*win_d, n=z_slow, axis=0)
 rd1_abs = np.abs(rd1)
@@ -135,17 +123,6 @@ ax.imshow(rd1_abs/np.max(rd1_abs),
           aspect='auto',
           origin='lower')
 
-# rd1 = np.fft.fft(rp1[int(0.5*N2):int(1.5*N2),:]*win_d, n=z_slow, axis=0)
-# rd1_abs = np.abs(rd1)
-
-# fig, ax = plt.subplots(figsize=(6,6))
-# ax.imshow(rd1_abs/np.max(rd1_abs), 
-#           cmap=plt.cm.Reds, 
-#           interpolation='none', 
-#           extent=[r_vec[0], r_vec[-1], v_vec[0], v_vec[-1]],
-#           aspect='auto',
-#           origin='lower')
-
 # for each i_T1 in slow time, but all n1 in fast time once
 for i_T1 in range(t_list.shape[0]):
     # one r & one v for each T1 chirp
@@ -153,11 +130,17 @@ for i_T1 in range(t_list.shape[0]):
     r[i_T1] = r0 + v[i_T1]*T1*i_T1
     
     tau = 2*(r[i_T1] + v[i_T1]*t_list_chirp) / c0
-    s[n2, :] = np.exp(
+    s1[n2, :] = np.exp(
         1j* 2* np.pi * (
             f0*tau + mu*tau*t_list_chirp - 0.5*mu*(tau**2)
             )
         )
+
+win_r = np.hanning(N1)
+win_r = np.tile(win_r, (t_list.shape[0],1))
+
+rp1 = np.fft.fft(s1*win_r, n=z_fast, axis=1)
+rp1_abs = np.abs(rp1)
 
 rd1 = np.fft.fft(rp1[0*N2:1*N2,:]*win_d, n=z_slow, axis=0)
 rd1_abs = np.abs(rd1)
